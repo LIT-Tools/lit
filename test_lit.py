5126806e96c416cf16d97bdd2c5e74f474ec4570
LEXX <s_kushnarev@rambler.ru>
2025-02-28 14:23:38 +0300

    refactor: changed examples of task names in tests

--- test_lit.py
+++ test_lit.py
@@ -33,12 +33,12 @@ class TestWorklogManager(unittest.TestCase):
         mock_datetime.now.return_value = self.MockedDateTime.now()
         mock_datetime.strptime.side_effect = lambda *args: datetime.strptime(*args)
 
-        self.manager.add_entry(['SOGAZ-123', '7', 'Описание работы'])
-        self.manager.add_entry(['SOGAZ-123', '7', 'Описание работы', '-d', '20.02.2024', '-t', '10:00'])
+        self.manager.add_entry(['TASK-123', '7', 'Описание работы'])
+        self.manager.add_entry(['TASK-123', '7', 'Описание работы', '-d', '20.02.2024', '-t', '10:00'])
 
         expected_content = (
-            "15.01.2023 [14:30 - 21:30] SOGAZ-123 7.0h `Описание работы`\n"
-            "20.02.2024 [10:00 - 17:00] SOGAZ-123 7.0h `Описание работы`\n"
+            "15.01.2023 [14:30 - 21:30] TASK-123 7.0h `Описание работы`\n"
+            "20.02.2024 [10:00 - 17:00] TASK-123 7.0h `Описание работы`\n"
         )
         mock_file().write.assert_called_with(expected_content)
 
@@ -54,11 +54,11 @@ class TestWorklogManager(unittest.TestCase):
         mock_input.return_value = 'y'
 
         # Добавляем запись
-        self.manager.add_entry(['SOGAZ-123', '2', 'Тестовая запись'])
+        self.manager.add_entry(['TASK-123', '2', 'Тестовая запись'])
         self.assertEqual(len(self.manager.entries), 1)
 
         # Проверяем что запись сохранилась в файл
-        expected_entry = "15.01.2023 [14:30 - 16:30] SOGAZ-123 2.0h `Тестовая запись`\n"
+        expected_entry = "15.01.2023 [14:30 - 16:30] TASK-123 2.0h `Тестовая запись`\n"
         mock_file().write.assert_called_with(expected_entry)
 
         # Выполняем push
@@ -73,7 +73,7 @@ class TestWorklogManager(unittest.TestCase):
         # Проверяем вывод перед подтверждением
         mock_print.assert_any_call("\nПодготовленные записи:")
         mock_print.assert_any_call("\n15.01.2023")
-        mock_print.assert_any_call("  SOGAZ-123 2.0h `Тестовая запись`")
+        mock_print.assert_any_call("  TASK-123 2.0h `Тестовая запись`")
         mock_print.assert_any_call("\nОтправка записей в Jira...")
         mock_print.assert_any_call("Записи успешно отправлены!")
 
@@ -90,11 +90,11 @@ class TestWorklogManager(unittest.TestCase):
         mock_input.return_value = 'n'
 
         # Добавляем запись
-        self.manager.add_entry(['SOGAZ-123', '2', 'Тестовая запись'])
+        self.manager.add_entry(['TASK-123', '2', 'Тестовая запись'])
         self.assertEqual(len(self.manager.entries), 1)
 
         # Проверяем что запись сохранилась в файл
-        expected_entry = "15.01.2023 [14:30 - 16:30] SOGAZ-123 2.0h `Тестовая запись`\n"
+        expected_entry = "15.01.2023 [14:30 - 16:30] TASK-123 2.0h `Тестовая запись`\n"
         mock_file().write.assert_called_with(expected_entry)
 
         # Выполняем push
@@ -104,13 +104,13 @@ class TestWorklogManager(unittest.TestCase):
         self.assertEqual(len(self.manager.entries), 1)
 
         # Проверяем что запись сохранилась в файл
-        expected_entry = "15.01.2023 [14:30 - 16:30] SOGAZ-123 2.0h `Тестовая запись`\n"
+        expected_entry = "15.01.2023 [14:30 - 16:30] TASK-123 2.0h `Тестовая запись`\n"
         mock_file().write.assert_called_with(expected_entry)
 
         # Проверяем вывод перед подтверждением
         mock_print.assert_any_call("\nПодготовленные записи:")
         mock_print.assert_any_call("\n15.01.2023")
-        mock_print.assert_any_call("  SOGAZ-123 2.0h `Тестовая запись`")
+        mock_print.assert_any_call("  TASK-123 2.0h `Тестовая запись`")
         mock_print.assert_any_call("Отмена отправки.")
 
 
@@ -124,7 +124,7 @@ class TestWorklogManager(unittest.TestCase):
 
         # Симулируем передачу аргументов из CLI (как если бы они были получены через vars(args))
         cli_args = {
-            'code': 'SOGAZ-123',
+            'code': 'TASK-123',
             'hours': 50.0,
             'message': 'Описание работы',
             'date': '15.01.2023',
@@ -135,7 +135,7 @@ class TestWorklogManager(unittest.TestCase):
         self.manager.add_entry(cli_args)
 
         # Расчет времени окончания: 15.01.2023 14:30 + 50 часов = 17.01.2023 16:30
-        expected_entry = "15.01.2023 [14:30 - 16:30] SOGAZ-123 50.0h `Описание работы`\n"
+        expected_entry = "15.01.2023 [14:30 - 16:30] TASK-123 50.0h `Описание работы`\n"
 
         # Проверяем, что запись корректно записана в файл
         mock_file().write.assert_called_with(expected_entry)
